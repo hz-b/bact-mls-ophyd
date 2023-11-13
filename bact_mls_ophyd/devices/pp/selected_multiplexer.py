77f267e60b95fedb6619af7b46b9c9f66e499817
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2023-11-13 20:00:33 +0100

    [TASK] removed bact2 mentioned in comment

--- bact_mls_ophyd/devices/pp/selected_multiplexer.py
+++ bact_mls_ophyd/devices/pp/selected_multiplexer.py
@@ -2,7 +2,6 @@ import threading
 import time
 
 from bact_bessyii_mls_ophyd.devices.utils.multiplexer_state_machine import MuxerState
-# from bact2.ophyd.devices.raw.quad_list import quadrupoles
 from ophyd import (Component as Cpt, EpicsSignal, EpicsSignalRO, Kind, PVPositionerPC, Signal, )
 from ophyd.status import AndStatus, SubscriptionStatus, Status
 
